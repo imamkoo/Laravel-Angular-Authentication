17ca9cb8aed8becc59bcc5e26dfe6ae9c82cf0f8
imamkoo <imammm.kooo@gmail.com>
2023-01-13 16:40:36 +0700

    Created Components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,7 +7,6 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { ProfileComponent } from './components/profile/profile.component';
-import { ResetPasswordComponent } from './components/password/reset-password/reset-password.component';
 import { RequestResetComponent } from './components/password/request-reset/request-reset.component';
 import { ResponseResetComponent } from './components/password/response-reset/response-reset.component';
 
@@ -18,15 +17,11 @@ import { ResponseResetComponent } from './components/password/response-reset/res
     LoginComponent,
     SignupComponent,
     ProfileComponent,
-    ResetPasswordComponent,
     RequestResetComponent,
-    ResponseResetComponent
+    ResponseResetComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
